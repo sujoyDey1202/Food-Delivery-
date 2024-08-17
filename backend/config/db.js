cb335c08e3ce830eacdca778abf2c069d5719beb
sujoyDey1202 <166387261+sujoyDey1202@users.noreply.github.com>
2024-08-17 10:26:01 +0530

    Delete backend/config/db.js

--- backend/config/db.js
+++
@@ -1,5 +0,0 @@
-import mongoose from "mongoose";
-
-export const connectDB = async()=>{
-    await mongoose.connect("mongodb+srv://dsujoy074:mongopass098@cluster0.msxhaph.mongodb.net/Food-delivery").then(()=>console.log("DB connected"));
-}
